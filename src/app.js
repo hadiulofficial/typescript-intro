8a791227f34f436b546f30e7ba0563f6628599e0
Hadiul Islam <hadiulislamofficial@gmail.com>
2023-02-15 07:36:44 +0000

    feat: array aond object type

--- src/app.js
+++ src/app.js
@@ -1,4 +1,26 @@
+var num1Input = document.getElementById('num1');
+var num2Input = document.getElementById('num2');
+var buttonEl = document.querySelector('button');
 function add(a, b) {
     return a + b;
 }
-console.log(add(3, 7));
+function printResult(result) {
+    console.log(result);
+}
+// const result  = add(5,8)
+// printResult(result)
+var results = [];
+buttonEl.addEventListener('click', function () {
+    var num1 = +num1Input.value;
+    var num2 = +num2Input.value;
+    var result = add(num1, num2);
+    var resultContainer = {
+        res: result,
+        print: function () {
+            console.log(this.res);
+        }
+    };
+    results.push(resultContainer);
+    printResult(results);
+    results[0].print();
+});
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,35 @@
+const num1Input = document.getElementById('num1') as HTMLInputElement
+const num2Input = document.getElementById('num2') as HTMLInputElement
+const buttonEl = document.querySelector('button') as HTMLInputElement
+
+
 function add(a: number, b: number){
     return a+b;
 }
 
-const result  = add(5,8)
+function printResult(result){
+    console.log(result);
+    
+}
 
-console.log(result)
+// const result  = add(5,8)
+
+// printResult(result)
+
+let results: {res: number, print: () => void}[] = []
+
+buttonEl.addEventListener('click', ()=>{
+    const num1 = +num1Input.value;
+    const num2 = +num2Input.value;
+    const result = add(num1, num2)
+    const resultContainer = {
+        res: result,
+        print () {
+            console.log(this.res);
+            
+        }
+    }
+    results.push(resultContainer)
+    printResult(results)
+    results[0].print()
+})
